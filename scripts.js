018fb4918865d0bcc92ec3cf1f98c48e57115dab
WPaulos <wendypls.pro@gmail.com>
2020-01-16 01:40:47 +0100

    Erreur JS, pas fini

--- scripts.js
+++ scripts.js
@@ -9,32 +9,37 @@ for (let i = 0; i < buttons.length; i++) {
 */
 
 const game = () => {
+  console.log("game");
   let pScore = 0;
   let cScore = 0;
 
   //Play match
   const playMatch = () => {
+    console.log("playmatch");
     const options = document.querySelectorAll(".option button");
     const playerHand = document.querySelector(".player-hand");
-    const computerHand = document.querySelector("computer-hand");
+    console.log(playerHand);
+    const computerHand = document.querySelector(".computer-hand");
     //Computer options
     const computerOptions = ["rock", "paper", "scissors"];
 
     options.forEach(option => {
       option.addEventListener("click", function() {
-        //Computer choice
         const computerNumber = Math.floor(Math.random() * 3);
-        const computerChoice = computerChoice[computerNumber];
+        const computerChoice = computerOptions[computerNumber];
         //Here is where we call compare hands
 
         //Update images
-        playerHand.scr = "./assets/img/img-white/${this.textContent}.png";
-        computerHand.scr = "./assets/img-white${computerChoice}.png";
+        console.log(playerHand);
+        playerHand.src = `./assets/img/img-white/${this.textContent}.png`;
+        computerHand.src = `./assets/img/img-white/${computerChoice}.png`;
       });
     });
+    compareHands();
   };
 
   const compareHands = (playerChoice, computerChoice) => {
+    console.log("compare");
     //Update text
     const rText = document.querySelectorAll(".result-text");
     //Checking for a tie
@@ -70,6 +75,8 @@ const game = () => {
       }
     }
   };
+
+  playMatch();
 };
 
 //star the game function
